b87bdaa1c99bf50e11abccb814480bf19b8c139e
marlonfajardo <marlonrfajardo@gmail.com>
2019-09-23 14:52:22 -0700

    Added 4 helper functions, get_input_base10(), convert(), find_remainder(), operation(), and format_conversion.

--- Lab 03/base_function.py
+++ Lab 03/base_function.py
@@ -1,6 +1,60 @@
+def get_input_base10()
+    """
+    :return: 
+    """
+
+
+
+
+def convert(user_input, user_destination):
+    """
+    Convert a base 10 number into base n.
+    :param user_input: int
+    :param user_destination: int
+    :return:
+    """
+    right_most = str(user_input % user_destination)
+    base_10 = int(user_input / user_destination)
+    right_middle = str(base_10 % user_destination)
+    base_10 = int(base_10 / user_destination)
+    left_middle = str(base_10 % user_destination)
+    base_10 = int(base_10 / user_destination)
+    left_most = str(base_10 % user_destination)
+    conversion = int(left_most + left_middle + right_middle + right_most)
+    return conversion
+
+
+def find_remainder():
+    """
+    Determine the remainder between two numbers.
+    :return: remainder as an int
+    """
+    right_most = str(user_input % user_destination)
+
+
+def operation():
+    """
+
+    :return:
+    """
+
+
+def format_conversion(Base_10,desired_base,conversion):
+    """
+    Print a string showing the determined conversion.
+    :param Base_10: int
+    :param desired_base: int
+    :param conversion: string
+    :return: a string containing the data collected.
+    """
+    conversion_phrase = (f"{Base_10} base 10 converted to base {desired_base} is: {conversion}")
+
+    return conversion_phrase
+
+
 def base_conversion():
     """
-    Convert a base 10 number to base n
+    Convert a base 10 number to base n.
     :postcondition: convert input to destination base.
     :return: base-n number
     """
@@ -8,17 +62,11 @@ def base_conversion():
     desired_base = int(input("Enter your destination base between 2 to 9:\n"))
     maximum_base_10 = 2 ** desired_base - 1
     maximum_base_10 = str(maximum_base_10)
-    Base_10 = int(input(f"Enter a Base 10 number less than or equal to {maximum_base_10}:\n"))
+    user_input = int(input(f"Enter a Base 10 number less than or equal to {maximum_base_10}:\n"))
+    convert()
+    format_conversion(user_input,desired_base)
 
 
-    right_most = str(Base_10 % desired_base)
-    base_10 = int(Base_10 / desired_base)
-    right_middle = str(base_10 % desired_base)
-    base_10 = int(base_10 / desired_base)
-    left_middle = str(base_10 % desired_base)
-    base_10 = int(base_10 / desired_base)
-    left_most = str(base_10 % desired_base)
-    conversion = int(left_most + left_middle + right_middle + right_most)
-    conversion_phrase = (f"{Base_10} base 10 converted to base {desired_base} is: {conversion}")
+if __name__ == __main__:
+    base_conversion()
 
-    return conversion_phrase
