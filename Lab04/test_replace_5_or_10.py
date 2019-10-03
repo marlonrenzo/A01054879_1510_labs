2814b29f4d03491b0f0f7ead92fc1bb5d4f0755b
marlonfajardo <marlonrfajardo@gmail.com>
2019-10-02 20:59:31 -0700

    Initial commit.

---
+++ Lab04/test_replace_5_or_10.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from Lab04.roman_numbers import replace_5_or_10
+
+class TestReplace_5_or_10(TestCase):
+    def test_replace_5_or_10(self):
+        actual_value = replace_5_or_10(10, "M")
+        expected_value = "MMMMMMMMMM"
+        self.assertEqual(actual_value, expected_value)
+        
