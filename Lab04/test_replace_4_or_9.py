b9ecb284c5c090b1548c0324f77f5a5312ba2ca7
marlonfajardo <marlonrfajardo@gmail.com>
2019-10-02 20:56:19 -0700

    Initial commit.

---
+++ Lab04/test_replace_4_or_9.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+from Lab04.roman_numbers import replace_4_or_9
+
+class TestReplace_4_or_9(TestCase):
+    def test_replace_4_or_9(self):
+        actual_value = replace_4_or_9(9, "X")
+        expected_value = "XC"
+        self.assertEqual(actual_value, expected_value)
