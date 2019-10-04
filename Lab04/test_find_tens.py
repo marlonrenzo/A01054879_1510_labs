de040f3dc67257a8078aa2ead95da1b72dd93224
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-03 22:53:38 -0700

    reworked to satisfy all boundaries

--- Lab04/test_find_tens.py
+++ Lab04/test_find_tens.py
@@ -2,7 +2,17 @@ from unittest import TestCase
 from Lab04.roman_numbers import find_tens
 
 class TestFind_tens(TestCase):
-    def test_find_tens(self):
-        actual_value = find_tens(87)
-        expected_value = 8
+    def test_find_tens_maximum(self):
+        actual_value = find_tens(99)
+        expected_value = 9
+        self.assertEqual(actual_value, expected_value)
+
+    def test_find_tens_under(self):
+        actual_value = find_tens(9)
+        expected_value = 0
+        self.assertEqual(actual_value, expected_value)
+
+    def test_find_tens_middle(self):
+        actual_value = find_tens(56)
+        expected_value = 5
         self.assertEqual(actual_value, expected_value)
