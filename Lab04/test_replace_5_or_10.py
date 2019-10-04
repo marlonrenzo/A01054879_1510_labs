4bb4ed57dbe7ebf045a95e1c5f31cbd83b000d6d
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-03 22:28:19 -0700

    reworked to satisfy all boundaries

--- Lab04/test_replace_5_or_10.py
+++ Lab04/test_replace_5_or_10.py
@@ -2,8 +2,14 @@ from unittest import TestCase
 from Lab04.roman_numbers import replace_5_or_10
 
 class TestReplace_5_or_10(TestCase):
+    def test_replace_5_or_10_V(self):
+        actual_value = replace_5_or_10(5, "I")
+        expected_value = "V"
+        self.assertEqual(actual_value, expected_value)
+
     def test_replace_5_or_10(self):
         actual_value = replace_5_or_10(10, "M")
         expected_value = "MMMMMMMMMM"
         self.assertEqual(actual_value, expected_value)
+
 
