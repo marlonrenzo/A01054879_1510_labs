29839b27e4873eaeb53bc27bde29021f36004b46
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-03 22:49:18 -0700

    reworked to satisfy all boundaries

--- Lab04/test_get_letters.py
+++ Lab04/test_get_letters.py
@@ -2,8 +2,24 @@ from unittest import TestCase
 from Lab04.roman_numbers import get_letters
 
 class TestGet_letters(TestCase):
-    def test_get_letters(self):
+    def test_get_letters_(self):
         actual_value = get_letters(3, "I")
         expected_value = "III"
         self.assertEqual(actual_value, expected_value)
+
+    def test_get_letters_800(self):
+        actual_value = get_letters(8, "C")
+        expected_value = "CCCCCCCC"
+        self.assertEqual(actual_value, expected_value)
+
+    def test_get_letters(self):
+        actual_value = get_letters(9, "I")
+        expected_value = "IX"
+        self.assertEqual(actual_value, expected_value)
+
+    def test_get_letters(self):
+        actual_value = get_letters(4, "C")
+        expected_value = "CD"
+        self.assertEqual(actual_value, expected_value)
+
 
