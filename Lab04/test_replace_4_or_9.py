21d330ad88719640c1741c7f42985cc94149ab07
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-03 22:33:42 -0700

    reworked to satisfy all boundaries

--- Lab04/test_replace_4_or_9.py
+++ Lab04/test_replace_4_or_9.py
@@ -2,7 +2,12 @@ from unittest import TestCase
 from Lab04.roman_numbers import replace_4_or_9
 
 class TestReplace_4_or_9(TestCase):
-    def test_replace_4_or_9(self):
+    def test_replace_4_or_9_400(self):
+        actual_value = replace_4_or_9(4, "C")
+        expected_value = "CD"
+        self.assertEqual(actual_value, expected_value)
+
+    def test_replace_4_or_9_nine(self):
         actual_value = replace_4_or_9(9, "X")
         expected_value = "XC"
         self.assertEqual(actual_value, expected_value)
