618c59d348aae121e68dacfa5cc195dab15b18bb
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-03 22:51:54 -0700

    reworked to satisfy all boundaries

--- Lab04/test_find_thousands.py
+++ Lab04/test_find_thousands.py
@@ -2,8 +2,18 @@ from unittest import TestCase
 from Lab04.roman_numbers import find_thousands
 
 class TestFind_thousands(TestCase):
-    def test_find_thousands(self):
-        actual_value = find_thousands(8796)
-        expected_value = 8
+    def test_find_thousands_maximum(self):
+        actual_value = find_thousands(9999)
+        expected_value = 9
+        self.assertEqual(actual_value, expected_value)
+
+    def test_find_thousands_under(self):
+        actual_value = find_thousands(999)
+        expected_value = 0
+        self.assertEqual(actual_value, expected_value)
+
+    def test_find_thousands_middle(self):
+        actual_value = find_thousands(3560)
+        expected_value = 3
         self.assertEqual(actual_value, expected_value)
 
