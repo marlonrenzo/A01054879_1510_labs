f5e5580d0d8860175c8ce33aa28071ff4b730d1d
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-10 08:55:09 -0700

    Carried main function over from lab05 file

---
+++ Lab05/game_driver.py
@@ -0,0 +1,16 @@
+def main():
+    """
+    Call all functions to run the program.
+    """
+    # inventory = ['Longsword', 'Dagger', 'Bow', 'Staff', 'Wand', 'Shield', 'Spear', 'Axe', 'Hammer']
+    # print(choose_inventory(inventory,2))
+    # print(generate_vowel())
+    # doctest.testmod()
+    # print(generate_name(4))
+    # print(roll_die(3,6))
+    # print(create_character(8))
+    print_character(create_character(8))
+
+
+if __name__ == '__main__':
+    main()
