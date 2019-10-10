3cbb074161b9d639ec786b42f0513edc7727cab1
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-10 16:16:37 -0700

    Proper test case created

--- Lab05/test_generate_consonant.py
+++ Lab05/test_generate_consonant.py
@@ -4,8 +4,7 @@ from unittest.mock import patch
 
 
 class TestGenerateConsonant(TestCase):
-    @patch("random.choice", return_value="t")
     def test_generate_consonant(self, mock_output):
-        expected_value = "t"
+        consonant = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
         actual_value = generate_consonant()
-        self.assertEqual(actual_value, expected_value)
+        self.assertTrue(actual_value in consonant)
