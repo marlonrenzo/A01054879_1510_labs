d0f0965c6eaf7ccc92097b6375bdef2704510688
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-10 09:57:20 -0700

    Unittest works properly

--- Lab05/test_roll_die.py
+++ Lab05/test_roll_die.py
@@ -4,8 +4,8 @@ from Lab05.lab05 import roll_die
 
 
 class TestRollDie(TestCase):
-    @patch("random.randint", return_value=8)
+    @patch("random.randint", return_value=2)
     def test_roll_die(self, mock_output):
-        actual_value = 8
-        expected_value = mock_output.getvalue()
+        expected_value = 8
+        actual_value = roll_die(4, 6)
         self.assertEqual(actual_value, expected_value)
