984d9d31d5f1c62af09fb3c08c9133a8a2596494
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-10 20:24:59 -0700

    Remove redundant tests, added choose_inventory implementation for character.

--- Lab05/game_driver.py
+++ Lab05/game_driver.py
@@ -1,9 +1,6 @@
 from Lab05.lab05 import roll_die
 from Lab05.lab05 import choose_inventory
 from Lab05.lab05 import generate_name
-from Lab05.lab05 import generate_vowel
-from Lab05.lab05 import generate_consonant
-from Lab05.lab05 import generate_syllable
 from Lab05.lab05 import create_character
 from Lab05.lab05 import print_character
 
@@ -16,13 +13,12 @@ def main():
     print("I am now rolling a 3d6 ---> You rolled", roll_die(3, 6))
     print("I am now generating two random inventory items for you ---> ", choose_inventory(inventory, 2))
     print("I am now generating your name ---> Hello", generate_name(4))
-    print("I am now generating a random vowel ---> ", generate_vowel())
-    print("I am now generating a random consonant ---> ", generate_consonant())
-    print("I am now generating a random syllable ---> ", generate_syllable())
     print("I am now creating a random character for you ---> ", create_character(8))
 
     print("\n... Here is your character info below ...")
-    print_character(create_character(8))
+    character = create_character(8)
+    character.append(choose_inventory(inventory, 2))
+    print_character(character)
 
 if __name__ == '__main__':
     main()
