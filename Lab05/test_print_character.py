d956571b28b47c0c0323433acf3b864b77285b23
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-11 00:17:46 -0700

    Final changes, test cases functional.

--- Lab05/test_print_character.py
+++ Lab05/test_print_character.py
@@ -9,7 +9,7 @@ class TestPrintCharacter(TestCase):
         char = ['Jacilogo', ['Strength', 7], ['Dexterity', 4], ['Constitution', 8], ['Intelligence', 14], ['Wisdom', 4], ['Charisma', 13]]
         print_character(char)
         actual_value = mock_output.getvalue()
-        expected_value = "\nName: Jacilogo\nStrength: 7\nDexterity: 4\nConstitution: 8\nIntelligence: 14\nWisdom: 4\nCharisma: 13\n"
+        expected_value = "Name: Jacilogo\nStrength: 7\nDexterity: 4\nConstitution: 8\nIntelligence: 14\nWisdom: 4\nCharisma: 13\n"
         self.assertEqual(actual_value, expected_value)
 
     @patch("sys.stdout", new_callable=io.StringIO)
@@ -20,7 +20,7 @@ class TestPrintCharacter(TestCase):
         char.append(dummy_inventory)
         print_character(char)
         actual_value = mock_output.getvalue()
-        expected_value = "\nName: Jacilogo\nStrength: 7\nDexterity: 4\nConstitution: 8\nIntelligence: 14\nWisdom: 4\nCharisma: 13\n--Here are your inventory items--\nsword\n"
+        expected_value = "Name: Jacilogo\nStrength: 7\nDexterity: 4\nConstitution: 8\nIntelligence: 14\nWisdom: 4\nCharisma: 13\n--Here are your inventory items--\nsword\n"
         self.assertEqual(actual_value, expected_value)
 
     @patch("sys.stdout", new_callable=io.StringIO)
