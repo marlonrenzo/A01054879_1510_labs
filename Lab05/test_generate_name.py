cbd08e0868d4350dbad1c554b2570e375e0e6fbd
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-10 20:42:59 -0700

    Finished test cases

--- Lab05/test_generate_name.py
+++ Lab05/test_generate_name.py
@@ -4,5 +4,17 @@ from unittest.mock import patch
 
 
 class TestGenerateName(TestCase):
-    def test_generate_name(self):
-        self.fail()
+    def test_generate_name_length(self):
+        actual_value = generate_name(18)
+        name_length = len(actual_value)
+        self.assertTrue(name_length == 36)
+
+    def test_generate_name_vowel(self):
+        vowel = ['a', 'e', 'i', 'o', 'u', 'y']
+        actual_value = generate_name(10)
+        self.assertTrue(actual_value[0] in vowel)
+
+    def test_generate_name_consonant(self):
+        consonant = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
+        actual_value = generate_name(10)
+        self.assertTrue(actual_value[4] in consonant)
