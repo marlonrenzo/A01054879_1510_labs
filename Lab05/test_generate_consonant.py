5be169b430216a43cc04a7e98439bb0faf419207
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-10 19:53:36 -0700

    Fixed typo

--- Lab05/test_generate_consonant.py
+++ Lab05/test_generate_consonant.py
@@ -3,7 +3,7 @@ from Lab05.lab05 import generate_consonant
 
 
 class TestGenerateConsonant(TestCase):
-    def test_generate_consonant(self, mock_output):
+    def test_generate_consonant(self):
         consonant = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
         actual_value = generate_consonant()
         self.assertTrue(actual_value in consonant)
