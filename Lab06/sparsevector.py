33bddf3501c99bb734c56785f1e89764cec23e7c
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-17 16:16:32 -0700

    program finished

--- Lab06/sparsevector.py
+++ Lab06/sparsevector.py
@@ -1,5 +1,22 @@
 def sparse_add(vector_one, vector_two):
-    
+    vector_sum = {}
+    for key in vector_one.keys():
+        if key not in vector_one.keys():
+            vector_sum[key] = vector_two[key]
+        elif key not in vector_two.keys():
+            vector_sum[key] = vector_one[key]
+        else:
+            vector_sum[key] = vector_one[key] + vector_two[key]
+
+    for key in vector_two.keys():
+        if key not in vector_one.keys():
+            vector_sum[key] = vector_two[key]
+        elif key not in vector_two.keys():
+            vector_sum[key] = vector_one[key]
+        else:
+            vector_sum[key] = vector_one[key] + vector_two[key]
+
+    return vector_sum
 
 
 def main():
