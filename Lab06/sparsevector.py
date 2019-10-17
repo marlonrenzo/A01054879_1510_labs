2d7ded954f75956cd700c9d8718f5948ec49d667
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-17 16:16:04 -0700

    Added main function

--- Lab06/sparsevector.py
+++ Lab06/sparsevector.py
@@ -1,2 +1,11 @@
-def sparse_add():
-    pass
+def sparse_add(vector_one, vector_two):
+    
+
+
+def main():
+    print(sparse_add({0: 1, 3: 4, 6: 5}, {2: 1, 4: 7, 10: 4}))
+
+
+if __name__ == "__main__":
+    main()
+
