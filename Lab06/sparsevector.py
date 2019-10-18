2256a280c361bdd96607846138911e8edda2373e
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-17 19:48:47 -0700

    added if statement covering cases that remove zeroes in the dictionaries

--- Lab06/sparsevector.py
+++ Lab06/sparsevector.py
@@ -1,9 +1,14 @@
 def sparse_add(vector_one, vector_two):
+    """
+    Add the common key values of two dictionaries and store the sum in a new dictionary.
+
+    :param vector_one: dictionary with integers
+    :param vector_two: dictionary with integers
+    :return: a dictionary with all common vectors added
+    """
     vector_sum = {}
     for key in vector_one.keys():
-        if key not in vector_one.keys():
-            vector_sum[key] = vector_two[key]
-        elif key not in vector_two.keys():
+        if key not in vector_two.keys():
             vector_sum[key] = vector_one[key]
         else:
             vector_sum[key] = vector_one[key] + vector_two[key]
@@ -11,11 +16,9 @@ def sparse_add(vector_one, vector_two):
     for key in vector_two.keys():
         if key not in vector_one.keys():
             vector_sum[key] = vector_two[key]
-        elif key not in vector_two.keys():
-            vector_sum[key] = vector_one[key]
-        else:
-            vector_sum[key] = vector_one[key] + vector_two[key]
-
+    for key in vector_sum:
+        if vector_sum[key] == 0:
+            del vector_sum[key]
     return vector_sum
 
 
