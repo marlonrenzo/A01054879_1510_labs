6718f40cf1d44c0760a54c8a0aeb4c967abee424
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-17 22:40:10 -0700

    Wrote doctests, modified code to remove elements with the value of zero

--- Lab06/sparsevector.py
+++ Lab06/sparsevector.py
@@ -1,3 +1,6 @@
+import doctest
+
+
 def sparse_add(vector_one, vector_two):
     """
     Add the common key values of two dictionaries and store the sum in a new dictionary.
@@ -5,6 +8,16 @@ def sparse_add(vector_one, vector_two):
     :param vector_one: dictionary with integers
     :param vector_two: dictionary with integers
     :return: a dictionary with all common vectors added
+    >>> sparse_add({}, {})
+    {}
+    >>> sparse_add({0: 4, 1: 5, 2: 7}, {0: 3, 1: 4, 3: 9})
+    {0: 7, 1: 9, 2: 7, 3: 9}
+    >>> sparse_add({0: -5, 4: -3}, {0: 5, 4: 3})
+    {}
+    >>> sparse_add({2: 4, 6: 8, 3: 5}, {})
+    {2: 4, 6: 8, 3: 5}
+    >>> sparse_add({0: -5, 4: -3}, {0: 5, 4: 3})
+    {}
     """
     vector_sum = {}
     for key in vector_one.keys():
@@ -16,13 +29,14 @@ def sparse_add(vector_one, vector_two):
     for key in vector_two.keys():
         if key not in vector_one.keys():
             vector_sum[key] = vector_two[key]
-    for key in vector_sum:
+    for key in list(vector_sum):
         if vector_sum[key] == 0:
             del vector_sum[key]
     return vector_sum
 
 
 def main():
+    doctest.testmod()
     print(sparse_add({0: 1, 3: 4, 6: 5}, {2: 1, 4: 7, 10: 4}))
 
 
