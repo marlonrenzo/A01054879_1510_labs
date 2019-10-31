9c7dac409e5c376780e843641568c340df2f3649
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-31 09:06:11 -0700

    Some test cases written

---
+++ Lab07/test_prepender.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+from Lab07.midterm import prepender
+
+
+class TestPrepender(TestCase):
+    def test_prepender_empty_list_and_empty_string(self):
+        actual_value = prepender([], "")
+        expected_value = []
+        self.assertEqual(actual_value, expected_value)
+
+    def test_prepender_empty_list_and_non_empty_string(self):
+        actual_value = prepender([], "Python")
+        expected_value = []
+        self.assertEqual(actual_value, expected_value)
+
+    def test_prepender(self):
+        actual_value = prepender([], "")
+        expected_value = []
+        self.assertEqual(actual_value, expected_value)
+
+    def test_prepender(self):
+        actual_value = prepender([], "")
+        expected_value = []
+        self.assertEqual(actual_value, expected_value)
+
+    def test_prepender(self):
+        actual_value = prepender([], "")
+        expected_value = []
+        self.assertEqual(actual_value, expected_value)
+
+    def test_prepender(self):
+        actual_value = prepender([], "")
+        expected_value = []
+        self.assertEqual(actual_value, expected_value)
+
+    def test_prepender(self):
+        actual_value = prepender([], "")
+        expected_value = []
+        self.assertEqual(actual_value, expected_value)
