43c6b442a0154ee2d6922fee0c4346cdd6c51e96
MarlonRenzo <marlonrfajardo@gmail.com>
2019-10-30 21:55:28 -0700

    Test cases created.

--- Lab07/test_list_tagger.py
+++ Lab07/test_list_tagger.py
@@ -1,6 +1,19 @@
 from unittest import TestCase
 from Lab07.midterm import list_tagger
 
-class TestList_tagger(TestCase):
-    def test_list_tagger(self):
-        self.fail()
+class TestListTagger(TestCase):
+    def test_list_tagger_empty_list(self):
+        actual_value = list_tagger([])
+        expected_value = [0]
+        self.assertEqual(actual_value, expected_value)
+
+    def test_list_tagger_one_element(self):
+        actual_value = list_tagger([1])
+        expected_value = [1, 1]
+        self.assertEqual(actual_value, expected_value)
+
+    def test_list_tagger_regular_list(self):
+        actual_value = list_tagger([1, 2, 3, 4, 5, 6])
+        expected_value = [6, 1, 2, 3, 4, 5, 6]
+        self.assertEqual(actual_value, expected_value)
+
