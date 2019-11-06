562735fd4df5c6055a42eb74366d81a7f3c3b245
MarlonRenzo <marlonrfajardo@gmail.com>
2019-11-06 11:09:44 -0800

    created test cases for all four corners of maze

--- Lab08/test_show_position.py
+++ Lab08/test_show_position.py
@@ -1,7 +1,50 @@
 from unittest import TestCase
 from Lab08.maze import show_position
+from unittest.mock import patch
+import io
 
 
 class TestShowPosition(TestCase):
-    def test_show_position(self):
-        actual_value = show_position()
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_show_position_top_left(self, mock_output):
+        show_position({"x": 0, "y": 0})
+        actual_value = mock_output.getvalue()
+        expected_value = '\n\n[x] [ ] [ ] [ ] [ ] \n\n' \
+                             '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                             '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                             '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                             '[ ] [ ] [ ] [ ] [ ] \n\n'
+        self.assertEqual(actual_value, expected_value)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_show_position_top_right(self, mock_output):
+        show_position({"x": 4, "y": 0})
+        actual_value = mock_output.getvalue()
+        expected_value = '\n\n[ ] [ ] [ ] [ ] [x] \n\n' \
+                         '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[ ] [ ] [ ] [ ] [ ] \n\n'
+        self.assertEqual(actual_value, expected_value)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_show_position_bottom_right(self, mock_output):
+        show_position({"x": 4, "y": 4})
+        actual_value = mock_output.getvalue()
+        expected_value = '\n\n[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[ ] [ ] [ ] [ ] [x] \n\n'
+        self.assertEqual(actual_value, expected_value)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_show_position_bottom_left(self, mock_output):
+        show_position({"x": 0, "y": 4})
+        actual_value = mock_output.getvalue()
+        expected_value = '\n\n[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[ ] [ ] [ ] [ ] [ ] \n\n' \
+                         '[x] [ ] [ ] [ ] [ ] \n\n'
+        self.assertEqual(actual_value, expected_value)
