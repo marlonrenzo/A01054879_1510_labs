b54a754ed2c1f845757dc702045a4fabb631b3e9
MarlonRenzo <marlonrfajardo@gmail.com>
2019-11-18 15:58:10 -0800

    3 test cases to test the rounding of the float

--- Lab10/test_print_result.py
+++ Lab10/test_print_result.py
@@ -7,7 +7,7 @@ import io
 
 class TestPrintResult(TestCase):
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_print_result(self, mock_output):
+    def test_print_result_prints_correctly(self, mock_output):
         food = get_food_items()
         total_test = 12345
         avg_test = 54321.1
@@ -17,4 +17,35 @@ class TestPrintResult(TestCase):
         actual = mock_output.getvalue()
         self.assertEqual(expected, actual)
 
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_print_result_average_rounds_to_one_decimal_place(self, mock_output):
+        food = get_food_items()
+        total_test = 12345
+        avg_test = 1.111111111111111111
+        keys = list(food.keys())
+        expected = f"\nFood Items: {sorted(keys)}\nTotal Calories: {total_test} Average Calories: 1.1\n\n"
+        print_result(keys, total_test, avg_test)
+        actual = mock_output.getvalue()
+        self.assertEqual(expected, actual)
 
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_print_result_average_rounds_up(self, mock_output):
+        food = get_food_items()
+        total_test = 12345
+        avg_test = 1.15999999999
+        keys = list(food.keys())
+        expected = f"\nFood Items: {sorted(keys)}\nTotal Calories: {total_test} Average Calories: 1.2\n\n"
+        print_result(keys, total_test, avg_test)
+        actual = mock_output.getvalue()
+        self.assertEqual(expected, actual)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_print_result_average_rounds_down(self, mock_output):
+        food = get_food_items()
+        total_test = 12345
+        avg_test = 1.149999999999
+        keys = list(food.keys())
+        expected = f"\nFood Items: {sorted(keys)}\nTotal Calories: {total_test} Average Calories: 1.1\n\n"
+        print_result(keys, total_test, avg_test)
+        actual = mock_output.getvalue()
+        self.assertEqual(expected, actual)
